050570e90ea61ef30309bfcd3c9224a33306667d
sara mager <sarahaya454@gmail.com>
2024-11-05 13:43:36 +0200

    intail code + welcom functions

---
+++ Stage0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome1454();
+        Console.ReadKey();
+        Welcome3700();
+    }
+    static partial void Welcome3700();
+    private static void Welcome1454()
+    {
+        Console.Write("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
