8809bf73d82be84d0203c8416fea219f3d995394
saramager <smager@g.jct.ac.il>
2024-11-05 21:52:34 +0200

    Update Class1.cs

--- Stage0/Class1.cs
+++ Stage0/Class1.cs
@@ -1,15 +1,2 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
-
-namespace Stage0
-{
-    internal class Class1
-    {
-       
-
-    }
-}
+using System.Collections.Generi
