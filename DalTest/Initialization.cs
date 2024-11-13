b44e0e316d2804d4ad9e975b4813e022e1990d6d
sara mager <sarahaya454@gmail.com>
2024-11-13 09:00:31 +0200

    change email

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -12,7 +12,7 @@ public static class Initialization
     private static  void createVolunteers()
     {
         string[] names = { "David Levy", "Miriam Cohen", "Yosef Katz", "Sarah Gold", "Shlomo Klein", "Chana Rosen", "Aharon Azulay", "Tova Benita", "Yaakov Pinto", "Esther Bar", "Dov Cohen", "Rivka Dayan", "Yitzhak Azulay", "Malkah Shalom", "Avraham Segal" };
-        string[] emails = { "david.levy@email.com", "miriam.cohen@gmail.com", "yosef.katz@outlook.com", "sarah.gold@yahoo.com", "shlomo.klein@live.com", "chana.rosen@icloud.com", "aharon.azulay@mail.com", "tova.benita@webmail.com", "yaakov.pinto@zoho.com", "esther.bar@aol.com", "dov.cohen@ymail.com", "rivka.dayan@fastmail.com", "yitzhak.azulay@protonmail.com", "malkah.shalom@gmx.com", "avraham.segal@tutanota.com" };
+        string[] emails = { "levy.david@email.com", "miriamCohen@gmail.com", "yosef.katz@outlook.com", "sarahGold789@yahoo.com", "shlomo.klein@live.com", "chana.rosen@icloud.com", "aharonAzulay@mail.com", "tova.benita@webmail.com", "yaakov.pinto@zoho.com", "esther.bar@aol.com", "dov.cohen@ymail.com", "rivka.dayan@fastmail.com", "yitzhak.azulay@protonmail.com", "malkah.shalom@gmx.com", "avraham.segal@tutanota.com" };
         string[] phoneNumbers = { "0521234567", "0532345678", "0543456789", "0554567890", "0585678901", "0596789012", "0577890123", "0588901234", "0529012345", "0530123456", "0541234567", "0552345678", "0583456789", "0574567890", "0595678901" };
         string adress[] = {}
         for (int i = 0; i < names.Length; i++)
