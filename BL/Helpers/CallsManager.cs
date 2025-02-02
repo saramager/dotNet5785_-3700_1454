42ce2848ddb973cc75c2bfca3af91c2ff9435c70
נעמי שבתאי <neomy@g.jct.ac.il>
2025-02-02 14:37:34 +0200

    timeEndCall

--- BL/Helpers/CallsManager.cs
+++ BL/Helpers/CallsManager.cs
@@ -65,7 +65,7 @@ namespace Helpers
                     CallId = doCall.ID,
                     callT = (BO.CallType)doCall.callT,
                     openTime = doCall.openTime,
-                    timeEndCall = (doCall.maxTime == null && lastAssignmentsForCall != null && lastAssignmentsForCall.finishT == DO.FinishType.Treated && lastAssignmentsForCall.finishT == DO.FinishType.ExpiredCancel) ? null : doCall.maxTime - s_dal.Config.Clock,
+                    timeEndCall = (doCall.maxTime == null || (lastAssignmentsForCall != null && (lastAssignmentsForCall.finishT == DO.FinishType.Treated || lastAssignmentsForCall.finishT == DO.FinishType.ExpiredCancel))) ? null : doCall.maxTime - s_dal.Config.Clock,
                     volunteerLast = (lastAssignmentsForCall != null) ? s_dal.Volunteer.Read(v => v.ID == lastAssignmentsForCall.VolunteerId)?.fullName : null,
                     TimeEndTreat = lastAssignmentsForCall?.finishTreatment != null ? lastAssignmentsForCall.finishTreatment - doCall.openTime : null,
                     status = GetCallStatus(doCall),
